d3cc8b93e49398afb0883410a843ab30abc9a32f
Piyush Srivastava <piyush061996@gmail.com>
2017-11-19 03:18:12 +0530

    First Commit

---
+++ ChatBot.java
@@ -0,0 +1,226 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import java.util.Date;
+import java.awt.Color;
+
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+
+import java.lang.Math;
+
+public class ChatBot extends JFrame implements KeyListener{
+
+	JPanel p=new JPanel();
+	JTextArea dialog=new JTextArea(20,50);
+	JTextArea input=new JTextArea(1,50);
+	JScrollPane scroll=new JScrollPane(
+		dialog,
+		JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+		JScrollPane.HORIZONTAL_SCROLLBAR_NEVER
+	);
+	
+	String[][] chatBot={
+		//standard greetings
+		{"hi","hello","hola","ola","howdy"},
+		{"hi","hello","hey"},
+		//question greetings
+		{"how are you","how r you","how r u","how are u"},
+		{"good","doing well"},
+		//yes
+		{"yes"},
+		{"no","NO","NO!!!!!!!"},
+		//default
+		{"shut up","you're bad","noob","stop talking",
+		"(michael is unavailable, due to LOL)"}
+	};
+	
+	public static void main(String[] args){
+		new ChatBot();
+	}
+	
+	public ChatBot(){
+		super("Chat Bot");
+		setSize(600,400);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		dialog.setEditable(false);
+		input.addKeyListener(this);
+	
+		p.add(scroll);
+		p.add(input);
+		p.setBackground(new Color(255,200,0));
+		add(p);
+		
+		setVisible(true);
+	}
+	
+	public void keyPressed(KeyEvent e){
+		if(e.getKeyCode()==KeyEvent.VK_ENTER){
+			input.setEditable(false);
+			//-----grab quote-----------
+			String quote=input.getText();
+			input.setText("");
+			addText("-->You:\t"+quote);
+			quote = quote.toLowerCase();
+			if((quote.matches("(.*)how(.*)")&&quote.matches("(.*)you(.*)")))
+			{
+				addText("\n-->Zo\t I'm good.");
+				try
+				{String[] env = {"PATH=/bin:/usr/bin/"};
+				String cmd = "say I'm good";  
+				Process process = Runtime.getRuntime().exec(cmd, env);
+				}
+				catch(Exception s) {
+        s.printStackTrace();
+        }
+			}
+
+			else if((quote.matches("(.*)your(.*)")&&quote.matches("(.*)name(.*)"))||(quote.matches("(.*)who(.*)")&&quote.matches("(.*)you(.*)")))
+			{
+				addText("\n-->Zo\t I'm Zo");
+				try
+				{String[] env = {"PATH=/bin:/usr/bin/"};
+				String cmd = "say I'm Zo";  
+				Process process = Runtime.getRuntime().exec(cmd, env);
+				}
+				catch(Exception s) {
+        s.printStackTrace();
+        }
+
+
+    }
+
+     	else if((quote.matches("(.*)hi(.*)"))||(quote.matches("(.*)hello(.*)"))||(quote.matches("(.*)hey(.*)")))
+     	{
+     		addText("\n-->Zo\t Hello");
+				try
+				{String[] env = {"PATH=/bin:/usr/bin/"};
+				String cmd = "say Hello";  
+				Process process = Runtime.getRuntime().exec(cmd, env);
+				}
+				catch(Exception s) {
+        				s.printStackTrace();
+        			}
+     	}
+
+        	else if((quote.matches("(.*)open(.*)")&&quote.matches("(.*)telegram(.*)")))
+			{
+				try
+				{String[] env = {"PATH=/bin:/usr/bin/"};
+				String cmd = "open /Applications/Telegram.app";  
+				Process process = Runtime.getRuntime().exec(cmd, env);
+				}
+				catch(Exception s) {
+        s.printStackTrace();
+        }
+
+			}
+
+			else if((quote.matches("(.*)play(.*)")&&quote.matches("(.*)music(.*)")))
+			{
+				try
+				{String[] env = {"PATH=/bin:/usr/bin/"};
+				String cmd = "open /Applications/iTunes.app";  
+				Process process = Runtime.getRuntime().exec(cmd, env);
+				}
+				catch(Exception s) {
+        s.printStackTrace();
+        }
+
+			}
+
+		else if(quote.matches("(.*)who(.*)")||(quote.matches("(.*)created(.*)")&&quote.matches("(.*)you(.*)")))
+			{
+				addText("\n-->Zo\t I was created by Piyush and Prajesh as a result of open ended project of Java");
+				try
+				{String[] env = {"PATH=/bin:/usr/bin/"};
+				String cmd = "say I was created by Piyush and Prajesh as a result of open ended project of Java";  
+				Process process = Runtime.getRuntime().exec(cmd, env);
+				}
+				catch(Exception s) {
+        				s.printStackTrace();
+        			}
+
+			}
+
+		else if((quote.matches("(.*)date(.*)"))||(quote.matches("(.*)time(.*)")))
+                      {
+                               Date date = new Date();
+                               addText("\n-->Michael\t Today's date is: " + date.toString());
+                           }
+            else{
+            	addText("\n-->Zo\t Sorry! You're expecting too much. I'm still under development.");
+				try
+				{String[] env = {"PATH=/bin:/usr/bin/"};
+				String cmd = "say Sorry! You're expecting too much. I'm still under development.";  
+				Process process = Runtime.getRuntime().exec(cmd, env);
+				}
+				catch(Exception s) {
+        				s.printStackTrace();
+        			}
+
+            }
+                             
+            quote.trim();
+			while(
+				quote.charAt(quote.length()-1)=='!' ||
+				quote.charAt(quote.length()-1)=='.' ||
+				quote.charAt(quote.length()-1)=='?'
+			){
+				quote=quote.substring(0,quote.length()-1);
+			}
+			quote.trim();
+			byte response=0;
+			/*
+			0:we're searching through chatBot[][] for matches
+			1:we didn't find anything
+			2:we did find something
+			*/
+			//-----check for matches----
+			// int j=0;//which group we're checking
+			// while(response==0){
+			// 	if(inArray(quote.toLowerCase(),chatBot[j*2])){
+			// 		response=2;
+			// 		int r=(int)Math.floor(Math.random()*chatBot[(j*2)+1].length);
+			// 		addText("\n-->Michael\t"+chatBot[(j*2)+1][r]);
+			// 	}
+			// 	j++;
+				// if(j*2==chatBot.length-1 && response==0){
+				// 	response=1;
+				// }
+			// }
+			
+			//-----default--------------
+			if(response==1){
+				int r=(int)Math.floor(Math.random()*chatBot[chatBot.length-1].length);
+				addText("\n-->Michael\t"+chatBot[chatBot.length-1][r]);
+			}
+			addText("\n");
+		}
+	}
+	
+	public void keyReleased(KeyEvent e){
+		if(e.getKeyCode()==KeyEvent.VK_ENTER){
+			input.setEditable(true);
+		}
+	}
+	
+	public void keyTyped(KeyEvent e){}
+	
+	public void addText(String str){
+		dialog.setText(dialog.getText()+str);
+	}
+	
+	public boolean inArray(String in,String[] str){
+		boolean match=false;
+		for(int i=0;i<str.length;i++){
+			if(str[i].equals(in)){
+				match=true;
+			}
+		}
+		return match;
+	}
+}
